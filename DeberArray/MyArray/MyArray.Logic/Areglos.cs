1cda54843cf94b6dec5a163976c6c9578e73e7db
Michu2005 <137965415+Michu2005@users.noreply.github.com>
2023-08-13 21:55:06 -0500

    Add files via upload

---
+++ DeberArray/MyArray/MyArray.Logic/Areglos.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArray.Logic
+{
+    public class Areglos
+    {
+        private int[] _array;
+        private int _top;
+
+        public Areglos(int n)
+        {
+            _array = new int[n]; //n posiciones
+            _top = 0; //tope ser cero
+            N = n; // mi parte fisica
+        }
+
+        public int N { get; } // Lectura
+
+        public bool IsFull => _top == N; // Arreglo lleno
+        public bool IsEmpty => _top == 0; // Arreglo vacio
+
+        public void Fill(int minimo, int maximo)
+        {
+            Random random = new Random();
+            
+            for (int contador = 0; contador < N; contador++)
+            {
+                _array[contador] = random.Next(minimo, maximo);
+            }
+            _top = N;
+        }
+
+        public Areglos NotRepeat()
+        {
+            int countNoRepat = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (!esRepetido(i))
+                {
+                    countNoRepat++;
+                }
+            }
+
+            var noRepeat = new Areglos(countNoRepat);
+
+            for (int i = 0; i < _top; i++)
+            {
+                if (!esRepetido(i))
+                {
+                    noRepeat.Add(_array[i]);
+                }
+
+            }
+
+            return noRepeat;
+        }
+
+        public Areglos Repeated()
+        {
+            int countRepated = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (esRepetido(i))
+                {
+                    countRepated++;
+                }
+            }
+
+            var Repeat = new Areglos(countRepated);
+
+            for (int i = 0; i < _top; i++)
+            {
+                if (esRepetido(i))
+                {
+                    Repeat.Add(_array[i]);
+                }
+            }
+
+            return Repeat;
+        }
+
+        private bool esRepetido(int i)
+        {            
+            bool isRepeat = false;
+            for (int j = 0; j < _top; j++)
+            {
+                if (i != j && _array[i] == _array[j])
+                {
+                    isRepeat = true;
+                    break;
+                }
+            }
+            return isRepeat;
+        }
+
+        public Areglos Primo()
+        {
+            int countPrimos = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrimo(_array[i]))
+                {
+                    countPrimos++;
+                }
+            }
+
+            var getIsPrimos  = new Areglos(countPrimos);
+
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrimo(_array[i]))
+                {
+                    getIsPrimos.Add(_array[i]);
+                }
+            }
+            return getIsPrimos;
+        }
+
+        public Areglos Pares()
+        {
+            int countPares = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i] % 2 == 0)
+                {
+                    countPares++;
+                }
+            }
+            var getPares = new Areglos(countPares);
+
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i] % 2 == 0)
+                {
+                    getPares.Add(_array[i]);
+                }
+            }
+
+            return getPares;
+        }
+
+        // sort(true) => ordene acendente
+        public void Sort()
+        {
+            Sort(false);
+        }
+
+        public int[] Get_array()
+        {
+            return _array;
+        }
+
+        public void Add(int n)
+        {
+            if (IsFull)
+            {
+                throw new Exception("Este arreglo esta lleno");
+            }
+            _array[_top] = n;
+            _top++;
+        }
+
+        public void Insertar(int position, int number)
+        {
+            if (IsFull)
+            {
+                throw new Exception("Este arreglo esta lleno");
+            }
+
+            if (position < 0) position = 0;
+            if (position > _top) position = _top;
+
+            for (int i = _top; i > position; i--)
+            {
+                _array[i] = _array[i - 1];
+            }
+            _array[position] = number;
+            _top++;
+        }
+
+        public void Sort(bool desc)
+        {
+            for (int i=0; i < _top - 1; i++)
+            {
+                for (int j = i + 1; j < _top; j++)
+                {
+                    var comparador = desc ? _array[i] < _array[j] : _array[i] > _array[j];
+                    if(comparador)  
+                    {
+                        Change(ref _array[i], ref _array[j]);
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (count > 9)
+                {
+                    output += "\n";
+                    count = 0;
+                }
+                output += $"{_array[i]}\t";
+                count++;
+            }
+            return output;
+        }
+
+        private void Change(ref int a, ref int b)
+        {
+            var aux = a;
+            a = b;
+            b = aux;
+        }
+
+        private bool IsPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            // Check from 2 to sqrt(n)
+            for (int i = 2; i < Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
+
---
+++ DeberArray/MyArray/MyArray.UI.Interfaz/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using MyArray.Logic;
+using System;
+
+try
+{
+    var continuar = true;
+    var array = new Areglos(10);
+    while (continuar)
+    {
+        Console.WriteLine($"El arreglo quedaria de la siguiente manera:\n {array}");
+        // 1. Imprimir mensaje que diga: ingrese la opcion que desea
+        Console.WriteLine("==============MENÚ PRINCIPAL=============");
+        Console.Write("Seleccione la opción que desea: \n\t");
+        Console.WriteLine(" 1. Agregar número \n\t 2. Insertar número \n\t 3. Llenar matriz \n\t 4. Números primos \n\t 5. Números pares \n\t 6. Números no repetidos \n\t 7. Números más repetidos");
+        Console.WriteLine("Ingrese la opción que desea: ");
+        // 2. Leer el valor ingresado por el usuario con readline
+        int opcion = int.Parse(Console.ReadLine());
+        // 3. Switch que llama a la opcion seleccionada
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Ingrese el número que desea agregar: ");
+                var numeroAgregado = Console.ReadLine();
+                // 3.1 mostrar el resultado de la opcion seleccionada
+                Console.WriteLine($"El número ingresado es: {numeroAgregado}");
+                if (numeroAgregado != null)
+                {
+                    array.Add(int.Parse(numeroAgregado));
+                }
+                break;
+            case 2:
+                Console.WriteLine("Ingrese la posicion que desea insertar: ");
+                var posicionInsertada = Console.ReadLine();
+                Console.WriteLine($"La posicion ingresado es: {posicionInsertada}");
+                Console.WriteLine("Ingrese el numero que desea insertar: ");
+                var numeroInsertado = Console.ReadLine();
+                Console.WriteLine($"La posición ingresado es: {numeroInsertado}");
+                if(posicionInsertada != null && numeroInsertado != null)
+                {
+                    array.Insertar(int.Parse(posicionInsertada), int.Parse(numeroInsertado));
+                }
+                
+                break;
+            case 3:
+                array.Fill(1, 100);
+                break;
+            case 4:
+                var getPrimos = array.Primo();
+                Console.WriteLine($"Los números primos son: {getPrimos.N} \n{getPrimos}");
+                break;
+            case 5:
+                var getPares = array.Pares();
+                Console.WriteLine($"Los números pares son: {getPares.N}\n {getPares}");
+                break;
+            case 6:
+                array.Sort();
+                var noRepeat = array.NotRepeat();
+                Console.WriteLine($"Los números que no se repiten son: {noRepeat.N}\n{noRepeat}");
+                break;
+            case 7:
+                array.Sort();
+                var repeat = array.Repeated();
+                Console.WriteLine($"Los números que más se repiten son: {repeat.N}\n{repeat}");
+                break;
+            default:
+                Console.WriteLine("La opción seleccionada no es válida");
+                break;
+        }
+
+        Console.WriteLine("***** Desea continuar [S/N] ****");
+        // 4 indicar si desea continuar S/N
+        Console.WriteLine("Ingrese su respuesta [S/N]: ");
+        var verificar = Console.ReadLine();
+        if ("N".Equals(verificar))
+        {
+            continuar = false;
+        }
+    }
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
